a689ea89c6554173214c9a302b38409a1056f185
Evgeni Dimitrov <evgeny.d.dimitrov@gmail.com>
2015-03-13 13:29:24 +0200

    added Adtemplate

---
+++ ADTemplate/ActiveDirectoryTemplate/ActiveDirectoryTemplate/AdTmpl.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.DirectoryServices;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ActiveDirectoryTemplate
+{
+    public class AdTmpl
+    {
+        public string SearchBase { get; set; }
+        public bool UserIntegratedSecurity { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+
+        public AdTmpl(string searchBase)
+        {
+            UserIntegratedSecurity = true;
+            SearchBase = searchBase;
+        }
+        public AdTmpl(string searchBase, string username, string password)
+        {
+            UserIntegratedSecurity = false;
+            SearchBase = searchBase;
+            Username = username;
+            Password = password;
+        }
+        public List<T> Search<T>(string filter, HashSet<string> propsToLoad, Func<SearchResult, T> mapper)
+        { 
+            DirectoryEntry entry = null;
+            if (UserIntegratedSecurity)
+            {
+                entry = new DirectoryEntry(SearchBase);
+            }
+            else
+            {
+                entry = new DirectoryEntry(SearchBase, Username, Password);
+            }
+
+            DirectorySearcher searcher = new DirectorySearcher(entry);
+            searcher.Filter = filter;
+            foreach (string prop in propsToLoad)
+            { 
+                searcher.PropertiesToLoad.Add(prop);
+            }
+
+            SearchResultCollection result = searcher.FindAll();
+            List<T> resultList = new List<T>();
+            foreach(SearchResult sr in result)
+            {
+                T resultObj = mapper.Invoke(sr);
+                resultList.Add(resultObj);
+            }
+
+            return resultList;
+        }
+        public static string GetProp(SearchResult rs, string propName)
+        {
+            ResultPropertyValueCollection propCol = rs.Properties[propName];
+            if (propCol != null && propCol.Count > 0)
+            {
+                return propCol[0].ToString();
+            }
+            return string.Empty;
+        }
+    }
+}
---
+++ ADTemplate/ActiveDirectoryTemplate/Client/Program.cs
@@ -0,0 +1,26 @@
+﻿using ActiveDirectoryTemplate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AdTmpl tmpl = new AdTmpl(@"LDAP://192.168.8.10/DC=csr,DC=nevexis,DC=com", @"Administrator", @"Mi4igan3");
+            List<User> u = tmpl.Search<User>("(&(objectClass=person)(sAMAccountName=EVGENI))", new HashSet<string> { AdProperties.LOGINNAME },
+                        a =>
+                        {
+                            return new User
+                                {
+                                    SamAccName = AdTmpl.GetProp(a, AdProperties.LOGINNAME)
+                                };
+                        });
+            Console.Write(u);
+        }
+    }
+}
